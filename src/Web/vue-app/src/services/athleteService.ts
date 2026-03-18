a39088a27352c82bfcd3989037c7517894821edb
JulienDuruisseau <judur001@edu.cegepgarneau.ca>
2026-03-18 16:38:30 -0400

    Ajout de l'indice de plaisir et des informations sur l'échelle d'effort et de plaisir

--- src/Web/vue-app/src/services/athleteService.ts
+++ src/Web/vue-app/src/services/athleteService.ts
@@ -77,12 +77,12 @@ export class AthleteService extends ApiService implements IAthleteService {
     return null
   }
 
-  public async submitSubmission(token: string, effort: number, durationMinutes: number): Promise<SucceededOrNotResponse> {
+  public async submitSubmission(token: string, effort: number, durationMinutes: number, pleasure?: number): Promise<SucceededOrNotResponse> {
     const response = await this
       ._httpClient
       .post<any, AxiosResponse<any>>(
         `${import.meta.env.VITE_API_BASE_URL}/athletes/submission`,
-        { token, effort, durationMinutes },
+        { token, effort, durationMinutes, pleasure },
         this.headersWithJsonContentType())
       .catch(function (error: AxiosError): AxiosResponse<any> {
         return error.response as AxiosResponse<any>
